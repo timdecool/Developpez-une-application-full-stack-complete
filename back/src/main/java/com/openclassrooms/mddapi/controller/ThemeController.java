d571cd4fc2dbbb8d32bee9d1edf10536da4f231b
timdecool <tim.decool@gmail.com>
2026-03-13 11:37:08 +0100

    read all themes

---
+++ back/src/main/java/com/openclassrooms/mddapi/controller/ThemeController.java
@@ -0,0 +1,30 @@
+package com.openclassrooms.mddapi.controller;
+
+import com.openclassrooms.mddapi.dto.ThemeDTO;
+import com.openclassrooms.mddapi.service.ThemeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/themes")
+public class ThemeController {
+
+    @Autowired
+    private ThemeService themeService;
+
+    @GetMapping("")
+    public ResponseEntity<List<ThemeDTO>> findAllThemes() {
+        List<ThemeDTO> themes = themeService.findAllThemes();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(themes)
+                ;
+    }
+
+}
--- back/src/test/java/com/openclassrooms/mddapi/MddApiApplicationTests.java
+++ back/src/test/java/com/openclassrooms/mddapi/MddApiApplicationTests.java
@@ -1,9 +1,11 @@
 package com.openclassrooms.mddapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class MddApiApplicationTests {
 
 	@Test
