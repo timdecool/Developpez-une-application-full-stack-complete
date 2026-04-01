a7a6f6e35f4bb4c119d61974a1d689f554d29223
timdecool <tim.decool@gmail.com>
2026-04-01 21:34:35 +0200

    theme read subscribe unsubscribe

--- front/src/app/pages/themes/themes.component.ts
+++ front/src/app/pages/themes/themes.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {HeaderComponent} from "../../shared/components/header/header.component";
+import {ThemeService} from "../../shared/services/theme.service";
+import {Theme} from "../../shared/models/Theme";
+import {MatCard, MatCardActions, MatCardContent, MatCardTitle} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-themes',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MatCard, MatCardTitle, MatCardContent, MatCardActions, MatButton],
   templateUrl: './themes.component.html',
   styleUrl: './themes.component.scss',
   standalone: true
 })
 export class ThemesComponent {
+
+  themeService = inject(ThemeService);
+
+  themes = signal<Theme[]>([]);
+
+  ngOnInit() {
+    this.loadThemes();
+  }
+
+  loadThemes(): void {
+    if (this.themes().length > 0) this.themes.set([]);
+    this.themeService.getThemes().subscribe(response => {
+      this.themes.set(response);
+    })
+  }
+
+  subscribeTheme(id: number): void {
+    this.themeService.subscribeTheme(id).subscribe(() => {
+      this.themes.update(
+        themes => themes.map(theme => theme.id === id ? { ...theme, subscribed: true }:theme)
+      );
+    })
+  }
 
 }
